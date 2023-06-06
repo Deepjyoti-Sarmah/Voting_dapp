8680ee9b6432d16f03823be6ab117a05923ece13
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-06 23:01:54 +0530

    added buttons and wagmi is giving error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,25 +2,78 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
+import AddCandidate from './Components/AddCandidate';
+import Voting from './Components/Voting';
+import { CONTRACT_ADDRESS, CONTRAACT_ABI } from './CONTRACT';
 
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useContract} from 'wagmi';
+import { useSigner } from '@rainbow-me/rainbow-kit/wagmi';
+
 
 function App() {
 
+  const [screen, setScreen] = useState("home")
+
+  const {address} = useAccount()
+
+  const {data: signer} = useSigner();
+  const contract = useContract({
+    address: CONTRACT_ADDRESS,
+    abi: CONTRAACT_ABI,
+    signerOrProvider: signer
+  })
+
+  console.log(contract);
+
+  const RenderScreen = () => {
+    return(
+      <div className='flex flex-col gap-4 items-center justify-center min-h-screen'> 
+      {
+        screen === 'addCandidate' ? (
+          <AddCandidate setScreen={setScreen} />
+        ) : (
+          <Voting setScreen={setScreen} />
+        )
+      }
+      </div>
+    )
+  } 
+  
   return (
     <>
       <div className="bg-black text-white">
         <div className="flex items-center justify-between flex-row px-4 py-2">
-          {/* Logo */}
           <h1 className="text-2xl font-bold">Election</h1>
           <ConnectButton />
         </div>
-        <div className="flex items-center justify-center min-h-screen">
-          <h1 className="text-4xl font-extrabold">Vote</h1>
-        </div>
+        {
+          screen === 'home' ? (
+
+            <div className="flex flex-col items-center justify-center min-h-screen">
+              <h1 className="text-4xl font-extrabold">Vote</h1>
+              {
+                address ? (
+                  <div className='flex flex-row gap-4 items-center justify-center'>
+                    <button 
+                    onClick={() => setScreen('addCandidate')}
+                    className='bg-blue-500 text-white px-4 py-2 rounded-lg'>Add Candidate</button>
+                    <button 
+                    onClick={() => setScreen('vote')}
+                    className='bg-blue-500 text-white px-4 py-2 rounded-lg'>Vote</button>
+                  </div>
+                ): (
+                  <ConnectButton />
+                )
+              }
+            </div>
+          ):(
+              <RenderScreen />
+            )
+        }
       </div>
     </>
   );
 }
 
-export default App
+export default App;
