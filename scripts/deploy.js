7b765f79f1850d185aad73b564e7b19248acb6eb
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-14 20:45:46 +0530

    adding deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,25 +4,52 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
+// const hre = require("hardhat");
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
+
+//   const lockedAmount = hre.ethers.parseEther("0.001");
+
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
+
+//   await lock.waitForDeployment();
+
+//   console.log(
+//     `Lock with ${ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
+
 const hre = require("hardhat");
 
+const Election = artifacts.require("./Election.sol");
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Deploy the Election contract
+  const election = await deployer.deploy(Election);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  // Get the address of the deployed contract
+  const electionAddress = election.address;
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  // Save the address of the deployed contract to a file
+  const path = `./deployments/election.address`;
+  await hre.writeFile(path, electionAddress);
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Log the address of the deployed contract to the console
+  console.log(`Election contract deployed to address: ${electionAddress}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
