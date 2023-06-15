c4bf7865b95322dfd3e5a0a3deb2fdf0660f1a93
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-15 20:44:35 +0530

    trying to resolve the deployment issue

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,51 +4,26 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-// const hre = require("hardhat");
-
-// async function main() {
-//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//   const unlockTime = currentTimestampInSeconds + 60;
-
-//   const lockedAmount = hre.ethers.parseEther("0.001");
-
-//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-//     value: lockedAmount,
-//   });
-
-//   await lock.waitForDeployment();
-
-//   console.log(
-//     `Lock with ${ethers.formatEther(
-//       lockedAmount
-//     )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-//   );
-// }
-
-// // We recommend this pattern to be able to use async/await everywhere
-// // and properly handle errors.
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
-
 const hre = require("hardhat");
 
 async function main() {
-  // Deploy the Election contract
-  const Election = await hre.ethers.getContractFactory("Election");
-  const election = await Election.deploy();
 
-  // Wait for the contract to be mined
-  await election.deployed();
-  // await election.deployTransaction.wait();
+  // const election = await hre.ethers.deployContract("Election");
 
-  // Get the address of the deployed contract
-  const electionAddress = election.address;
+  // await election.waitForDeployment();
 
-  // Log the address of the deployed contract to the console
-  console.log(`Election contract deployed to address: ${electionAddress}`);
+   // Deploy the Election contract
+   const Election = await hre.ethers.getContractFactory("Election");
+   const election = await Election.deploy();
+ 
+   // Wait for the contract to be mined
+   await election.deployTransaction.wait();
+ 
+   // Get the address of the deployed contract
+   const electionAddress = election.address;
+ 
+   // Log the address of the deployed contract to the console
+   console.log(`Election contract deployed to address: ${electionAddress}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
