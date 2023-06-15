a6f04aac28e6387b9eece73ab49fb9b79821f270
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-15 22:46:47 +0530

    fixed the deployment script , reverted hardhat to 2.13.0 and changed package.json file , used web3_storage_dapp file help

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,29 +1,25 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
+
 const hre = require("hardhat");
 
 async function main() {
 
-  // const election = await hre.ethers.deployContract("Election");
+  const [deployer] = await ethers.getSigners();
 
-  // await election.waitForDeployment();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
    // Deploy the Election contract
-   const Election = await hre.ethers.getContractFactory("Election");
-   const election = await Election.deploy();
- 
+    const Election = await hre.ethers.getContractFactory("Election");
+    const election = await Election.deploy();
+
    // Wait for the contract to be mined
-   await election.deployTransaction.wait();
- 
+    await election.deployed();
+
    // Get the address of the deployed contract
-   const electionAddress = election.address;
- 
+    const electionAddress = election.address;
+
    // Log the address of the deployed contract to the console
-   console.log(`Election contract deployed to address: ${electionAddress}`);
+    console.log(`Election contract deployed to address: ${electionAddress}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -32,3 +28,8 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+
+// Deploying contracts with the account: 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+// Account balance: 9999997257860520395232
+// Election contract deployed to address: 0x5FC8d32690cc91D4c39d9d3abcBD16989F875707
