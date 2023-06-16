65443c23c16679acd576a85830c5ba35197e9f49
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-16 12:09:13 +0530

    added test script

--- test/election.test.js
+++ test/election.test.js
@@ -3,105 +3,49 @@
 //   loadFixture,
 // } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 // const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-// const { expect } = require("chai");
+const { expect } = require("chai");
+const hre = require("hardhat");
 
-// const hre = require("hardhat");
+describe("Election", function() {
+    let electionContract;
+    let owner;
+    let addr1;
+    let addr2;
 
-// describe("Election", function () {
+    beforeEach(async function() {
+        //Deploy the contract
+        const Election = await hre.ethers.getContractFactory("Election");
+        electionContract = await Election.deploy();
+        await electionContract.deployed();
 
-//   let election;
+        //Get signers
+        [owner, addr1, addr2] = await hre.ethers.getSigners();
+    });
 
-//   beforeEach(async function () {
-//     // Deploy the Election contract
-//     election = await hre.ethers.getContractFactory("Election");
-//     election = await election.deploy();
+    it("should have an initial candidate count of zero", async function () {
+        const candidateCount = await electionContract.candidateCount();
+        expect(candidateCount).to.equal(0);
+    })
 
-//     await election.deployed()
-//   });
+    it("should allow the owner to add candidate", async function() {
+        await electionContract.connect(owner).addCandidate("Jack", "Party 1", "image.png");
+        const candidateCount = await electionContract.candidateCount();
+        expect(candidateCount).to.equal(1);
+    });
 
-//   it("should add a candidate", async function () {
-//     // Add a candidate
-//     const name = "John Doe";
-//     const party = "Democrat";
-//     const imageUri = "https://example.com/image.png";
-//     await election.addCandidate(name, party, imageUri);
+    it("should not allow non-owners to add a candidate", async function() {
+        await expect(electionContract.connect(addr1).addCandidate("Jack", "Party 2", "image.png")).to.be.revertedWith("Not the owner");
+    });
 
-//     // Check that the candidate was added
-//     const candidate = await election.candidates(1);
-//     expect(candidate.name).toEqual(name);
-//     expect(candidate.party).toEqual(party);
-//     expect(candidate.imageUri).toEqual(imageUri);
-//   });
+    it("should increment the vote count when a candidate is voted for", async function() {
+        await electionContract.connect(owner).addCandidate("Jack", "Party 3", "image.png");
+        await electionContract.connect(addr1).vote(1);
+        const totalVotes = await electionContract.totalVotes();
+        expect(totalVotes).to.equal(1);
+    });
 
-//   it("should vote for a candidate", async function () {
-//     // Add a candidate
-//     const name = "John Doe";
-//     const party = "Democrat";
-//     const imageUri = "https://example.com/image.png";
-//     await election.addCandidate(name, party, imageUri);
-
-//     // Vote for the candidate
-//     await election.vote(1);
-
-//     // Check that the vote was counted
-//     const votes = await election.votes(1);
-//     expect(votes).toEqual(1);
-//   });
-
-// });
-// describe("Election", function () {
-//   let election;
-//   let owner;
-//   const candidateName = "Candidate 1";
-//   const candidateParty = "Party 1";
-//   const candidateImageUri = "image_uri_1";
-
-//   beforeEach(async function () {
-//     const Election = await hre.ethers.getContractFactory("Election");
-//     election = await Election.deploy();
-//     await election.deployed();
-
-//     [owner] = await ethers.getSigners();
-//   });
-
-//   it("should add a candidate", async function () {
-//     // Add a candidate
-//     await election.addCandidate(candidateName, candidateParty, candidateImageUri);
-
-//     // Get the candidate details
-//     const candidate = await election.candidates(1);
-
-//     // Verify the candidate details
-//     expect(candidate.name).to.equal(candidateName);
-//     expect(candidate.party).to.equal(candidateParty);
-//     expect(candidate.imageUri).to.equal(candidateImageUri);
-//   });
-
-//   it("should increment the vote count", async function () {
-//     // Add a candidate
-//     await election.addCandidate(candidateName, candidateParty, candidateImageUri);
-
-//     // Vote for the candidate
-//     await election.vote(1);
-
-//     // Get the vote count for the candidate
-//     const voteCount = await election.votes(1);
-
-//     // Verify the vote count
-//     expect(voteCount).to.equal(1);
-//   });
-
-//   it("should increment the total vote count", async function () {
-//     // Add a candidate
-//     await election.addCandidate(candidateName, candidateParty, candidateImageUri);
-
-//     // Vote for the candidate
-//     await election.vote(1);
-
-//     // Get the total vote count
-//     const totalVotes = await election.totalVotes();
-
-//     // Verify the total vote count
-//     expect(totalVotes).to.equal(1);
-//   });
-// });
+    it("should not allow for non-existing candidates", async function() {
+        await expect(electionContract.connect(addr1).vote(1)).to.be.revertedWith("Candidate doesn't exist");
+    })
+    
+})
