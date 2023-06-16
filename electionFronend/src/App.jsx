59a9df0f30f92c4c568d2d268ee93045d2450266
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-06-16 16:29:35 +0530

    rainbowkit connected and working

--- electionFronend/src/App.jsx
+++ electionFronend/src/App.jsx
@@ -3,17 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 
-// import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center min-h-screen bg-black text-white'>
-        <h1 className='text-4xl font-extrabold'>Election Dapp</h1>
-        <ConnectButton />
+      <div className="bg-black text-white">
+        <div className="flex items-center justify-between flex-row px-4 py-2">
+          <h1 className="text-2xl font-bold">Election</h1>
+          <ConnectButton />
+        </div>
+        <div className="flex items-center justify-center min-h-screen">
+          <h1 className="text-4xl font-extrabold">Vote</h1>
+        </div>
       </div>
-      
     </>
   )
 }
--- electionFronend/src/main.jsx
+++ electionFronend/src/main.jsx
@@ -3,33 +3,36 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// import '@rainbow-me/rainbowkit/styles.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createConfig, sepolia, WagmiConfig } from 'wagmi';
+import { mainnet, polygonMumbai, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
-// import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-// import { configureChains, createClient, WagmiConfig } from 'wagmi';
-// import { mainnet, polygonMumbai, optimism, arbitrum } from 'wagmi/chains';
-// import { alchemyProvider } from 'wagmi/providers/alchemy';
-// import { publicProvider } from 'wagmi/providers/public';
 
+const { chains, publicClient } = configureChains(
+  [polygonMumbai, sepolia],
+  [
+    alchemyProvider({ apiKey: import.meta.env.ALCHEMY_ID }),
+    publicProvider()
+  ]
+);
 
-// const { chains, provider } = configureChains(
-//   [polygonMumbai],
-//   [
-//     alchemyProvider({ apiKey: import.meta.env.REACT_APP_ALCHEMY_ID }),
-//     publicProvider()
-//   ]
-// );
+const { connectors } = getDefaultWallets({
+  appName: 'Election Dapp',
+  projectId: 'YOUR_PROJECT_ID',
+  chains
+});
 
-// const { connectors } = getDefaultWallets({
-//   appName: 'FileStorage',
-//   chains
-// });
-
-// const wagmiClient = createClient({
-//   autoConnect: true,
-//   connectors,
-//   provider
-// })
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient
+})
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -38,6 +41,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <RainbowKitProvider chains={chains}>
         <App />
       </RainbowKitProvider>
-    </WagmiConfig> 
+    </WagmiConfig>
   </React.StrictMode>,
 );
